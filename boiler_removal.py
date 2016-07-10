472ca20ded7500615ef4a7cc3d071382d03a7ab0
Andrew Moore <a.moore@lancs.ac.uk>
2016-07-10 15:58:44 +0100

    Added exception handling on the number of command line arguments and added docstrings to the script.

--- boiler_removal.py
+++ boiler_removal.py
@@ -1,13 +1,33 @@
+'''Removes the boiler plate from a folder of HTML files and outputs them as
+text files in a given output directory. Takes two command line arguments:
+1) The path to the folder of HTML files.
+2) The path or name of a folder where the text files will be stored NOTE that
+the folder does not have to exist.
+'''
+
 import sys
 import subprocess
 import os
 import re
 
+__author__ = "Andrew Moore"
+__email__ = "a.moore@lancaster.ac.uk"
+
+if len(sys.argv) != 3:
+    exception = """ The script takes 2 arguments:\n
+    1) The path to the folder containing HTML files.\n
+    2) The path or name of the folder you would like the boiler plate removed
+    HTML files to be stored as text files (Note the folder does not have to exist)
+    """
+    raise Exception(exception)
+
 results_folder = sys.argv[1]
+output_directory = sys.argv[2]
+
 if not os.path.exists(results_folder):
     raise Exception("The result folder %s does not exist" %results_folder)
 results_folder = os.path.abspath(results_folder)
-output_directory = sys.argv[2]
+
 if not os.path.exists(output_directory):
     os.mkdir(output_directory)
 output_directory = os.path.abspath(output_directory)
