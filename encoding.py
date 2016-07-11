e89be9ca87868a65827508a77226a1b9ea1b3158
Andrew Moore <a.moore@lancs.ac.uk>
2016-07-11 01:48:06 +0100

    Added docstrings and comments. Now encodes the data, detects according to metadata and from the text and then gives stats in the log of the differences between the metadata data encoding and from the text.

--- encoding.py
+++ encoding.py
@@ -1,4 +1,40 @@
+''' Given two command line arguments will identfy the encoding of the HTML files
+using the metadata from when the files were downloaded and using a package that
+identifies the encoding from the text. Then encodes the files to UTF-8 and
+displays the statistics of the difference between the stated metadata encoding
+and the encoding infered from the text. Two command line arguments:
+1) The path to the folder containing HTML files.\n
+2) The filename of the database used to contain the metadata.
+'''
+
+# Standard modules
+import logging
+import os
 import sqlite3 as lite
+import sys
+# Non-standard modules
+from chardet.universaldetector import UniversalDetector
+
+__author__ = "Andrew Moore"
+__email__  = "a.moore@lancaster.ac.uk"
+
+def detected_encoding(page):
+    '''Given a file object will return a dictionary of encodings and confidence values.
+
+    Reference:
+    https://chardet.readthedocs.io/en/latest/usage.html
+    '''
+
+    detector = UniversalDetector()
+
+    for line in page.readlines():
+        detector.feed(line)
+        if detector.done:
+            break
+
+    detector.close()
+    return detector.result
+
 
 def get_charset(database):
     '''Given the database filepath will return a Cursor interator that contains
@@ -10,3 +46,89 @@ def get_charset(database):
     result = cursor.execute(sql)
 
     return result
+
+
+logging.basicConfig(format='%(asctime)s %(message)s', filename='encoding.log', level=logging.INFO)
+
+if len(sys.argv) != 3:
+    exception = """ The script takes 2 arguments:\n
+    1) The path to the folder containing HTML files.\n
+    2) The filename of the database used to contain the metadata.
+    """
+    logging.debug(exception)
+    raise Exception(exception)
+
+results_folder = sys.argv[1]
+database_name  = sys.argv[2]
+
+if not os.path.exists(results_folder):
+    logging.debug("The result folder %s does not exist" %results_folder)
+    raise Exception("The result folder %s does not exist" %results_folder)
+results_folder = os.path.abspath(results_folder)
+
+database_path = os.path.join(results_folder, database_name)
+if not os.path.exists(database_path):
+    logging.debug("The database %s does not exist" %database_path)
+    raise Exception("The database %s does not exist" %database_path)
+
+difference_stats = {}
+
+for row in get_charset(database_path):
+    url_id, charset = row
+    html_file = os.path.join(results_folder, str(url_id) + ".html")
+
+    logging.info("Processing %s file." % html_file)
+
+    guessed_encoding = None
+    with open(html_file, 'rb') as f:
+        guessed_encoding = detected_encoding(f)
+    if guessed_encoding == None:
+        print("Could not read the following file %s." %html_file)
+        continue
+
+    guessed_confidence = guessed_encoding['confidence']
+    guessed_encoding   = guessed_encoding['encoding']
+    logging.info("Guessed the following encoding %s with %1.2f confidence." %  (guessed_encoding, guessed_confidence))
+
+    html_data = None
+
+    if charset == None:
+        logging.info("Charset defined by metadata does not exist.")
+    else:
+        logging.info("Charset defined by metadata %s." % charset)
+
+        with(open(html_file, mode='r', encoding=charset)) as f:
+            html_data = f.read()
+
+    # Convert HTML to UTF-8
+    with(open(html_file, encoding='utf-8', mode='r')) as f:
+        logging.info("Read file as UTF-8 data for re-encoding to UTF-8.")
+        html_data = f.read()
+    # Write the converted data back to the the same file
+    with(open(html_file, encoding='utf-8', mode='w')) as f:
+        logging.info("Wrote back to file with UTF-8 data.")
+        f.write(html_data)
+
+    # See if the metadata encoding statement is different to the guessed
+    # encoding from the data.
+    if charset.lower() != guessed_encoding.lower():
+        if charset not in difference_stats:
+            difference_stats[charset] = {}
+        if guessed_encoding not in difference_stats[charset]:
+            difference_stats[charset][guessed_encoding] = 1
+        else:
+            difference_stats[charset][guessed_encoding] += 1
+
+# Logging the stats of number of differences between the guessed encoding
+# and the encoding specified within the page metadata.
+for metadata_charset, gussed_stats in difference_stats.items():
+    outer_log = """ The following number of times the encodings were gussed from the data
+    which according to the metadata collected from the HTML headers should be %s:
+    \n
+    """
+    logging.info(outer_log % metadata_charset)
+
+    for guessed_charset, num_times in gussed_stats.items():
+        inner_log = """guessed charset: %s number of times gussed differently
+        with regards to metadata charset: %d"""
+        logging.info(inner_log % (guessed_charset, num_times))
